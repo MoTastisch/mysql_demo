88cc68ede4c4a75411a64747da4684f493f6c844
MoTastish <the.minecrafter.business@gmail.com>
2025-10-13 11:39:42 +0200

    Create database.php
    added config

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+// database.php - externe Datenbank-Konfiguration
+return [
+    'host' => 'mysql',
+    'dbname' => 'website',
+    'username' => 'root',
+    'password' => 'demo1234',
+    'charset' => 'utf8mb4',
+];
